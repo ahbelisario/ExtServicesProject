f2c7603764c96fe6a266aab66e7b8d928315299b
Arturo Hernandez <Arturo.Hernandez.B@ibm.com>
2025-12-12 16:47:22 -0600

    with passwd

---
+++ pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://arturos-air.hdez.cloud:5555/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "12-12-2025 13:06:58 CST";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"ExtServicesProject","displayName":"","status":"Development","tags":[""],"createdDate":"01-12-2025 11:28:42 CST","version":"1.1","buildNumber":"","description":""};
+}
+});
